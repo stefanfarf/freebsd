95aa3b0da08d608a7b2ac87eff30a5388fda71db
Stefan Farfeleder <stefanf@FreeBSD.org>
2015-07-06 21:33:46 +0200

    Apply Linux 31d8d651eb646498413abe5f17acc7fa2b68b2fc.

--- sys/dev/drm2/i915/i915_gem.c
+++ sys/dev/drm2/i915/i915_gem.c
@@ -2787,10 +2787,8 @@ i915_gem_object_unbind(struct drm_i915_gem_object *obj)
 	ret = 0;
 	if (obj->gtt_space == NULL)
 		return (0);
-	if (obj->pin_count != 0) {
-		DRM_ERROR("Attempting to unbind pinned buffer\n");
-		return (-EINVAL);
-	}
+	if (obj->pin_count)
+		return -EBUSY;
 
 	ret = i915_gem_object_finish_gpu(obj);
 	if (ret == -ERESTARTSYS || ret == -EINTR)
